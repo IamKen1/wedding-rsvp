2244dc3b6e53a87f3b4bc46c182381d9c5195f06
IamKen1 <108377112+IamKen1@users.noreply.github.com>
2024-12-07 12:58:11 +0800

    Update motion.ts

--- src/types/motion.ts
+++ src/types/motion.ts
@@ -4,12 +4,19 @@ import { motion, MotionProps } from "framer-motion";
 type HTMLMotionProps<T extends keyof HTMLElementTagNameMap> = 
   Omit<React.HTMLAttributes<HTMLElementTagNameMap[T]>, keyof MotionProps> & MotionProps;
 
+// Special type for input elements
+type InputMotionProps = Omit<React.InputHTMLAttributes<HTMLInputElement>, keyof MotionProps> & MotionProps;
+type TextareaMotionProps = Omit<React.TextareaHTMLAttributes<HTMLTextAreaElement>, keyof MotionProps> & MotionProps;
+type SelectMotionProps = Omit<React.SelectHTMLAttributes<HTMLSelectElement>, keyof MotionProps> & MotionProps;
+
 // Motion components with proper typing
 export const MotionDiv = motion.div as React.FC<HTMLMotionProps<"div">>;
 export const MotionSection = motion.section as React.FC<HTMLMotionProps<"section">>;
 export const MotionForm = motion.form as React.FC<HTMLMotionProps<"form">>;
-export const MotionInput = motion.input as React.FC<HTMLMotionProps<"input">>;
-export const MotionTextarea = motion.textarea as React.FC<HTMLMotionProps<"textarea">>;
-export const MotionSelect = motion.select as React.FC<HTMLMotionProps<"select">>;
 export const MotionP = motion.p as React.FC<HTMLMotionProps<"p">>;
-export const MotionButton = motion.button as React.FC<HTMLMotionProps<"button">>; 
+export const MotionButton = motion.button as React.FC<HTMLMotionProps<"button">>;
+
+// Form element components with specific props
+export const MotionInput = motion.input as React.FC<InputMotionProps>;
+export const MotionTextarea = motion.textarea as React.FC<TextareaMotionProps>;
+export const MotionSelect = motion.select as React.FC<SelectMotionProps>; 
